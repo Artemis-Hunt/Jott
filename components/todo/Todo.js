2647a7a3ebb16c9bdd5880059c24695304d2b7f0
xianan2 <65153699+xianan2@users.noreply.github.com>
2021-02-27 23:09:56 +0800

    Update Todo.js

--- components/todo/Todo.js
+++ components/todo/Todo.js
@@ -4,15 +4,59 @@ import {
     TouchableOpacity,
     Text,
     View,
+    Keyboard,
+    TextInput,
+    Touchable
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-let Note = () => {
+let TodoList = () => {
 
     return(
-        <View>
-            
+        <View style = {TodoListStyles.MainContainer}>
+          <View style = {TodoListStyles.TitleSection}> 
+            <Text style = {TodoListStyles.TitleText}>{TodoList.Title}</Text>
+          </View>
+          <View style={TodoListStyles.BodySection}>
+            <Text style={TodoListStyles.BodyText}>{TodoList.Text}</Text>
+          </View>   
         </View>
     )
 }
 
-export default Note;
+const TodoListStyles = StyleSheet.create({
+    MainContainer:{
+        flex:1,
+        justifyContent: 'center',
+        backgroundColor: '#EEE'
+    },
+    TitleSection:{
+        flex:0.2,
+        backgroundColor: 'dodgerblue',
+        justifyContent: 'center',
+        paddingLeft:10,
+        paddingRight:10,
+        marginBottom:5
+    },
+    TitleText:{
+      fontSize: 20,
+      fontWeight: "bold",
+      fontFamily: 'Roboto'
+    },
+    BodySection:{
+      flex:0.8,
+      backgroundColor: 'deepskyblue',
+      alignContent: '',
+      paddingLeft:10,
+      paddingRight:10,
+      marginTop:5
+    },
+    BodyText:{
+      fontSize: 14,
+      fontWeight: "normal",
+      fontFamily: "Roboto"
+    }
+});
+export default TodoList;
