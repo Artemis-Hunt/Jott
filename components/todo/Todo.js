eb9f0f18b968c3b9529fa1f5852b8fa3016bb54c
tianyilim <0.tianyi.lim@gmail.com>
2021-02-27 21:27:34 +0800

    Cleanup Repo for Split Dev

---
+++ components/todo/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    View,
+} from 'react-native';
+
+let Note = () => {
+
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
+export default Note;
